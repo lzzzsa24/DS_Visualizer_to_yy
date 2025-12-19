ed883205b67d73ee95e2e69b21f015fc6c0e15a4
lzz24 <lzz24@mails.tsinghua.edu.cn>
2025-12-19 23:56:02 +0800

    add game sound effects

--- src/game/game_controller.py
+++ src/game/game_controller.py
@@ -1,11 +1,37 @@
-from PyQt6.QtCore import Qt, QObject,QTimer
+from PyQt6.QtCore import Qt, QObject,QTimer,QUrl
 from src.game.game_model import GameModel
 from src.game.game_view import GameView
 from src.model.exceptions import StructureFullError,StructureEmptyError
+from PyQt6.QtMultimedia import QSoundEffect
+import os
 
 class GameController(QObject):
     def __init__(self, view: GameView):
         super().__init__()
+
+        #初始化音效
+        self.push_sound = QSoundEffect()
+        self.pop_sound = QSoundEffect()
+        self.error_sound = QSoundEffect()
+        self.step_sound = QSoundEffect()
+        current_dir = os.path.dirname(os.path.abspath(__file__)) # 获取当前文件所在目录
+        project_root = os.path.dirname(os.path.dirname(current_dir)) # 往上跳两级：src -> 根目录
+        sounds_dir = os.path.join(project_root, 'resources', 'sounds')
+
+        push_sound_path = os.path.join(sounds_dir, 'add_element_successfully.wav')
+        pop_sound_path = os.path.join(sounds_dir, 'remove_element_successfully.wav')
+        error_sound_path = os.path.join(sounds_dir, 'error.wav')
+        step_sound_path = os.path.join(sounds_dir, 'step.wav')
+        self.push_sound.setSource(QUrl.fromLocalFile(push_sound_path))
+        self.pop_sound.setSource(QUrl.fromLocalFile(pop_sound_path))
+        self.error_sound.setSource(QUrl.fromLocalFile(error_sound_path))
+        self.step_sound.setSource(QUrl.fromLocalFile(step_sound_path))
+        self.push_sound.setVolume(0.5)  # 设置音量
+        self.pop_sound.setVolume(4.0)
+        self.error_sound.setVolume(0.3)
+        self.step_sound.setVolume(0.8)
+
+        # 初始化 MVC 组件
         self.view = view
         self.model = GameModel() 
 
@@ -110,16 +136,19 @@ class GameController(QObject):
         # Case 1:  墙 (1)
         if target_val == 1:
             self.model.message = " 墙壁太硬了，撞不开！"
+            self.error_sound.play()
 
         # Case 2:  门 (8) -> 通关判定
         elif target_val == 8:
             if top_item == 5:  # 需要 Key (5)
                 self.model.message = " 门打开了！下一层..."
                 self.model.backpack.pop()
+                self.pop_sound.play()
                 if not self.model.next_level():
                     self.model.message = " 恭喜通关！所有关卡完成！"
             else:
                 self.model.message = " 门锁着，你需要钥匙！"
+                self.error_sound.play()
         # Case 3:  怪物 (7) -> 战斗判定
         elif target_val == 7:
             if top_item == 4:  # 需要 Sword (4)
@@ -127,8 +156,10 @@ class GameController(QObject):
                 self.model.grid[target_y][target_x] = 0 # 怪物消失
                 self.model.move_player(dx, dy)
                 self.model.backpack.pop() 
+                self.pop_sound.play()
             else:
                 self.trigger_death(" 你被怪物吃掉了！")
+                self.error_sound.play()
 
         # Case 4:  火焰 (6) -> 消耗品判定
         elif target_val == 6:
@@ -138,8 +169,10 @@ class GameController(QObject):
                 self.model.grid[target_y][target_x] = 0 # 火消失
                 self.model.move_player(dx, dy)
                 self.model.backpack.pop() 
+                self.pop_sound.play()
             else:
                 self.trigger_death(" 你被烧焦了！")
+                self.error_sound.play()
 
         # Case 5:  道具 (3水, 4剑, 5匙) -> 拾取逻辑
         elif target_val in [3, 4, 5]:
@@ -151,14 +184,17 @@ class GameController(QObject):
                 self.model.message = f"你获得了 {item_names[target_val]}"
                 self.model.grid[target_y][target_x] = 0 # 地面变空
                 self.model.move_player(dx, dy)
+                self.push_sound.play()
             except StructureFullError:
                 # 失败后 (捕获异常)
                 self.model.message = " 背包满了！装不下！"
+                self.error_sound.play()
 
         # Case 6: 空地 (0) -> 直接移动
         else:
             self.model.move_player(dx, dy)
             self.model.message = "栈，移动！"
+            self.step_sound.play()
 
         # 刷新界面
         self.refresh_view()
